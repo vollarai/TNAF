bdd938c4fc614c20fe08f965c6fe6256885f789c
vollarai <raivolha@gmail.com>
2024-11-21 08:51:27 +0100

    functions

---
+++ zaj21_11/zad1/functions.js
@@ -0,0 +1,25 @@
+function isOdd(x)
+{
+    if(x%2==0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+function isEven(x)
+{
+    if(x%2==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+console.log(isEven(5));
